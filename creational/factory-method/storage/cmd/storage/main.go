921ea362119660898cd5a7edebc5fa8db3f2d5bd
alok87 <alok.singh@practo.com>
2017-09-28 17:55:54 +0530

    Addded one more storage impl

--- creational/factory-method/storage/cmd/storage/main.go
+++ creational/factory-method/storage/cmd/storage/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-    "github.com/alok87/go-design-patterns/creational/factory-method/storage"
+	"github.com/alok87/go-design-patterns/creational/factory-method/storage"
 )
-
 
 func main() {
 
-    diskStorage := storage.NewStore(storage.Disk)
-    diskStorage.Write("disco")
-    diskStorage.Read()
+	diskStorage := storage.NewStore(storage.Disk)
+	diskStorage.Write("disco")
+	diskStorage.Read()
 
+	memStorage := storage.NewStore(storage.Memory)
+	memStorage.Write("khisko")
+	memStorage.Read()
 }
